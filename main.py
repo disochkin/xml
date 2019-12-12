37d88295c109519ce8c863c4e064dd9ccd9633e4
Denis Nagovitsyn <disochkin@gmail.com>
2019-12-12 22:57:41 +0500

    add file

--- main.py
+++ main.py
@@ -1,22 +1,60 @@
+import xml.etree.cElementTree as ET
+from collections import Counter
+import json
+
+
 def get_list_top(string, symbol_count=5, quantity=3):
     result = ""
     list_big_word = {}
     for word in string.split(" "):
         if len(word) > symbol_count:
-            list_big_word.update({word: list_big_word.get(word, 0) + 1})
+            list_big_word.update({word.lower(): list_big_word.get(word, 0) + 1})
     list_for_sort = list(list_big_word.items())
     list_for_sort.sort(key=lambda i: i[1], reverse=True)
-    for item, _ in zip(list_for_sort, range(quantity)):
-        result = result + " " + item[0]
+    return get_numbered_elements(list_for_sort, quantity)
+
+
+def get_numbered_elements(source_list,quantity=10):
+    result = ""
+    i = 0
+    for item, _ in zip(source_list, range(quantity)):
+        i += 1
+        result = result + str(i) + "." + item[0] + " " + str(item[1]) + "\n"
     return result
 
 
-import xml.etree.cElementTree as ET
+def get_list_top2(string, symbol_count=5, quantity=3):
+    def remove_short(x):
+        if len(x) > symbol_count:
+            return x
 
-tree = ET.parse("import.xml")
-root = tree.getroot()
-items = root.findall("channel/item")
-words_array = ""
-for item in items:
-        words_array = words_array + " " + item.find("description").text
-print(get_list_top(words_array, 6, 10))
+    value_counts = Counter(list(filter(remove_short, string.split(" "))))
+    return get_numbered_elements(value_counts.most_common(), quantity)
+
+
+def get_data_from_json(file):
+    with open(file, 'r', encoding='utf-8') as fh:
+        data = json.load(fh)
+        words_array = ""
+        for item in data["rss"]["channel"]["items"]:
+            for word in item["description"].split(" "):
+                words_array = words_array + " " + word
+    return words_array
+
+def get_data_from_xml(file):
+    tree = ET.parse(file)
+    root = tree.getroot()
+    items = root.findall("channel/item")
+    words_array = ""
+    for item in items:
+        temp = item.find("description").text
+        words_array = words_array + temp.lower()
+    return words_array
+
+print(get_list_top(get_data_from_xml("import.xml"), 6, 10))
+print(get_list_top(get_data_from_json("import.json"), 6, 10))
+
+#добавил второй вариант. сомневаюсь на счет читаемости кода.
+print(get_list_top2(get_data_from_xml("import.xml"), 6, 10))
+print(get_list_top2(get_data_from_json("import.json"), 6, 10))
+
