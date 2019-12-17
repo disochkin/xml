be20550af260c0ddf08b4dd8eb537584e3516b7c
Denis Nagovitsyn <disochkin@gmail.com>
2019-12-17 23:00:38 +0500

    add file

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import xml.etree.cElementTree as ET
 from collections import Counter
 import json
 
+from prettytable import PrettyTable
 
 def get_list_top(string, symbol_count=5, quantity=3):
-    result = ""
     list_big_word = {}
     for word in string.split(" "):
         if len(word) > symbol_count:
@@ -14,12 +14,16 @@ def get_list_top(string, symbol_count=5, quantity=3):
     return get_numbered_elements(list_for_sort, quantity)
 
 
-def get_numbered_elements(source_list,quantity=10):
-    result = ""
+def custom_print(table_data):
+    for line in table_data:
+        print(line[0], "cлово: ",line[1], "встретилось", line [2], " раз")
+
+def get_numbered_elements(source_list, quantity=10):
+    result = []
     i = 0
     for item, _ in zip(source_list, range(quantity)):
         i += 1
-        result = result + str(i) + "." + item[0] + " " + "\n"
+        result.append((i, item[0], item[1]))
     return result
 
 
@@ -38,8 +42,9 @@ def get_data_from_json(file):
         words_array = ""
         for item in data["rss"]["channel"]["items"]:
             for word in item["description"].split(" "):
-                words_array = words_array + " " + word
+                words_array = words_array + " " + word.lower()
     return words_array
+
 
 def get_data_from_xml(file):
     tree = ET.parse(file)
@@ -51,10 +56,13 @@ def get_data_from_xml(file):
         words_array = words_array + temp.lower()
     return words_array
 
-print(get_list_top(get_data_from_xml("import.xml"), 6, 10))
-print(get_list_top(get_data_from_json("import.json"), 6, 10))
+print("")
+custom_print(get_list_top(get_data_from_xml("import.xml"), 6, 10))
+print("")
+custom_print(get_list_top(get_data_from_json("import.json"), 6, 10))
 
-#добавил второй вариант. сомневаюсь на счет читаемости кода. нашел на хабре - решил попробовать.
-print(get_list_top2(get_data_from_xml("import.xml"), 6, 10))
-print(get_list_top2(get_data_from_json("import.json"), 6, 10))
-
+# добавил второй вариант. сомневаюсь на счет читаемости кода. нашел на хабре - решил попробовать.
+print("")
+custom_print(get_list_top2(get_data_from_xml("import.xml"), 6, 10))
+print("")
+custom_print(get_list_top2(get_data_from_json("import.json"), 6, 10))
